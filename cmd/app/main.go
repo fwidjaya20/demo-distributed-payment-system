4c12dae9986b76c481c0c578860d4438c33592d3
Fredrick Widjaya <f.widjaya20@gmail.com>
2019-09-25 13:58:18 +0700

    fix config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,30 +4,20 @@ import (
 	"fmt"
 	"github.com/fwidjaya20/demo-distributed-payment-system/cmd/nats"
 	stan "github.com/nats-io/go-nats-streaming"
-	"log"
 	"runtime"
-	"time"
 )
 
 func main() {
 	natsConn := nats.InitNatsConnection()
 
-	aw, _ := time.ParseDuration("60s")
-	s, _ := natsConn.Subscribe("OrderSystem.Order", func(msg *stan.Msg) {
-		_ = msg.Ack()
+	s, err := natsConn.Subscribe("OrderSystem.Order", func(msg *stan.Msg) {
+		fmt.Println(msg.Data)
+	})
 
-		log.Println("Message", msg.Data)
+	if nil != err{
+		_ = s.Unsubscribe()
+		panic(err)
+	}
 
-		go func() {
-			fmt.Println("Sleep Time")
-			time.Sleep(10 * time.Second)
-		}()
-	},
-		stan.DurableName("payment-system-durable"),
-		stan.SetManualAckMode(),
-		stan.AckWait(aw),
-	)
-
-	_ = s.Unsubscribe()
 	runtime.Goexit()
 }
--- cmd/nats/service.go
+++ cmd/nats/service.go
@@ -1,6 +1,7 @@
 package nats
 
 import (
+	"fmt"
 	"github.com/fwidjaya20/demo-distributed-payment-system/config"
 	"github.com/nats-io/go-nats"
 	stan "github.com/nats-io/go-nats-streaming"
@@ -12,6 +13,8 @@ func InitNatsConnection() stan.Conn {
 	cluster := config.GetEnv(config.EVENT_STORE_NATS_CLUSTER)
 	natsClientId := config.GetEnv(config.NATS_CLIENT_ID)
 	natsAddr := config.GetEnv(config.EVENT_STORE_NATS_ADDR)
+
+	fmt.Println(cluster, natsClientId, natsAddr)
 
 	nc, err := nats.Connect(natsAddr)
 
